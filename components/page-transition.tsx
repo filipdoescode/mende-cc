0ba01dd795e247076ffdb4fd1a7b86dfe6b35450
filipdoescode <vasilovskif@gmail.com>
2024-11-25 19:47:15 +0100

    added page transition

---
+++ components/page-transition.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+interface PageTransitionProps {
+  children: React.ReactNode
+}
+
+export function PageTransition({ children }: PageTransitionProps) {
+  return (
+    <motion.div
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.2 }}
+    >
+      {children}
+    </motion.div>
+  )
+}
